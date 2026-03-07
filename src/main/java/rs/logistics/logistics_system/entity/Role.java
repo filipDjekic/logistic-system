b0991c77beb95299dc987b0bbd4804e4bf8d703d
Filip Đekić <e14filipdjekic@gmail.com>
2026-03-07 13:39:41 +0100

    Role entity created

---
+++ src/main/java/rs/logistics/logistics_system/entity/Role.java
@@ -0,0 +1,28 @@
+package rs.logistics.logistics_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import rs.logistics.logistics_system.enums.RoleName;
+
+@Entity
+@Table(name = "ROLE")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private RoleName name;
+
+    public Role(RoleName name){
+        this.name = name;
+    }
+}
---
+++ src/main/java/rs/logistics/logistics_system/enums/RoleName.java
@@ -0,0 +1,8 @@
+package rs.logistics.logistics_system.enums;
+
+public enum RoleName {
+    ADMIN,
+    MANAGER,
+    DISPATCHER,
+    WAREHOUSE_WORKER
+}
