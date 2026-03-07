7cc4c84f89e85cc71fd815ab99fe054c5590d8f6
Filip Đekić <e14filipdjekic@gmail.com>
2026-03-07 05:53:02 +0100

    Setup

---
+++ src/test/java/rs/logistics/logistics_system/LogisticsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.logistics.logistics_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
