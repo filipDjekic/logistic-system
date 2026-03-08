47d50905d54dd651a044d1d29a9a2c725af75571
Filip Đekić <e14filipdjekic@gmail.com>
2026-03-08 17:35:15 +0100

    Vehicle repo

---
+++ src/main/java/rs/logistics/logistics_system/repository/VehicleRepository.java
@@ -0,0 +1,21 @@
+package rs.logistics.logistics_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rs.logistics.logistics_system.entity.Vehicle;
+import rs.logistics.logistics_system.entity.Warehouse;
+import rs.logistics.logistics_system.enums.ShiftStatus;
+import rs.logistics.logistics_system.enums.WarehouseStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    Optional<Vehicle> findByRegistrationNumber(String registrationNumber);
+
+    boolean existsByRegistrationNumber(String registrationNumber);
+
+    List<Vehicle> findByStatus(ShiftStatus status);
+
+    List<Vehicle> findByActive(Boolean active);
+}
